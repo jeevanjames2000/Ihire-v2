b4b01cdc0f856f810231dca774cd50c685734b1b
muthyala10 <muthylanayudu@meetowner.in>
2025-10-10 15:24:32 +0530

    Added space only

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -20,6 +20,7 @@ const buttonVariants = cva(
           "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
         link: "text-primary underline-offset-4 hover:underline",
       },
+      
       size: {
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
         sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
